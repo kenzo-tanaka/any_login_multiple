09cdfdc9b67cd8ac8ecea46f030ad41c5334453d
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-21 19:12:31 +0900

    Remove some files.

--- app/helpers/easy_login/application_helper.rb
+++
@@ -1,4 +0,0 @@
-module EasyLogin
-  module ApplicationHelper
-  end
-end
--- app/models/easy_login/application_record.rb
+++
@@ -1,5 +0,0 @@
-module EasyLogin
-  class ApplicationRecord < ActiveRecord::Base
-    self.abstract_class = true
-  end
-end
