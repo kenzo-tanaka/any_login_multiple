1371b2506e344a5f0d07e0ad7009b4164156abf9
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-22 11:46:36 +0900

    Remove klass_name

--- lib/easy_login.rb
+++ lib/easy_login.rb
@@ -3,21 +3,13 @@ require "easy_login/engine"
 
 module EasyLogin
   extend ActiveSupport::Autoload
-
   autoload :Helpers
 
   mattr_accessor :klass_names
   @@klass_names = ['User']
 
-  mattr_accessor :klass_name
-
   def self.klasses
     @@klasses = EasyLogin.klass_names.map(&:constantize)
-  end
-
-  # TODO: remove me
-  def self.klass
-    @@klass = EasyLogin.klass_name.constantize
   end
 end
 
