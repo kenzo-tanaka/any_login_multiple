e74a286f48ec884c07fb55bfdf24745d2823628e
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-22 11:26:51 +0900

    Enable login by passing user id.

--- app/controllers/easy_login/application_controller.rb
+++ app/controllers/easy_login/application_controller.rb
@@ -1,7 +1,7 @@
 module EasyLogin
   class ApplicationController < ActionController::Base
     def easy_login
-      @user = EasyLogin.klass.find(2)
+      @user = EasyLogin.klass.find(params[:id])
       sign_in :user, @user
 
       redirect_to main_app.root_path
