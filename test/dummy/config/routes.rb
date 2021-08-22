58cbb95b2da12606deb75d5302678ceac1b29124
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-22 14:45:16 +0900

    Generate staff model in dummy dir.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :staffs
   devise_for :users
   root to: 'welcome#index'
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_054238) do
+ActiveRecord::Schema.define(version: 2021_08_22_054440) do
+
+  create_table "staffs", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_staffs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_staffs_on_reset_password_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
