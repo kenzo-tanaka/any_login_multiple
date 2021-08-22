396fa25e8005301ea0c292702f59463535511e51
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-22 11:28:35 +0900

    Add comment about sign in method.

--- app/controllers/easy_login/application_controller.rb
+++ app/controllers/easy_login/application_controller.rb
@@ -2,6 +2,8 @@ module EasyLogin
   class ApplicationController < ActionController::Base
     def easy_login
       @user = EasyLogin.klass.find(params[:id])
+
+      # Call devise sign in method
       sign_in :user, @user
 
       redirect_to main_app.root_path
