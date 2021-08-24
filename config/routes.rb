02aa12926f1b77654eb6d494926b2587666daec3
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-24 11:21:26 +0900

    Fix routing name not to conflict other routing name.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 AnyLoginMultiple::Engine.routes.draw do
-  post '/any_login_multiple/sign_in', to: 'application#any_login_multiple', as: :sign_in
+  post '/any_login_multiple/sign_in', to: 'application#any_login_multiple', as: :any_login_multiple_sign_in
 end
